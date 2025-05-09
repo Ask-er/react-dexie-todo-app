93b23c57d26e52efd2de3f7bb02583eb3ad9c0d1
Ask-er <hasankln45200@outlook.com>
2025-05-10 01:45:25 +0300

    react router changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,52 +8,57 @@ import { Navigate } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            path: "myday",
+            element: <MyDayPage />,
+          },
+          {
+            path: "tasks",
+            errorElement: <Error />,
+            children: [
+              {
+                path: "",
+                element: <Navigate to="/myday" replace />,
+              },
+              {
+                path: "next-7-days",
+                element: <Next7TodoPage />,
+              },
+              {
+                path: "lists/:listTitle",
+                element: <ListDetailPage />,
+                loader: loadList,
+                children: [
+                  {
+                    path: "tasks",
+                    element: <Navigate to=".." replace relative="path" />,
+                  },
+                  {
+                    path: "tasks/:todoId",
+                    element: <ItemDetailPage />,
+                    loader: loadTodo,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            path: "/",
+            element: <Navigate to="/myday" replace />,
+          },
+        ],
+      },
+    ],
     {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "myday",
-          element: <MyDayPage />,
-        },
-        {
-          path: "tasks",
-          errorElement: <Error />,
-          children: [
-            {
-              path: "",
-              element: <Navigate to="/myday" replace />,
-            },
-            {
-              path: "next-7-days",
-              element: <Next7TodoPage />,
-            },
-            {
-              path: "lists/:listTitle",
-              element: <ListDetailPage />,
-              loader: loadList,
-              children: [
-                {
-                  path: "tasks",
-                  element: <Navigate to=".." replace relative="path" />,
-                },
-                {
-                  path: "tasks/:todoId",
-                  element: <ItemDetailPage />,
-                  loader: loadTodo,
-                },
-              ],
-            },
-          ],
-        },
-        {
-          path: "/",
-          element: <Navigate to="/myday" replace />,
-        },
-      ],
-    },
-  ]);
+      basename: "/react-dexie-todo-app",
+    }
+  );
 
   return <RouterProvider router={router} />;
 }
